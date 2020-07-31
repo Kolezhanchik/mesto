87a0342431246bb182f066778d0ab8d4f5731e5d
Elena <lenhenlady@gmail.com>
2020-07-31 21:20:57 +0300

    first review corrections update

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,33 @@
 
+let form = document.querySelector('.popup__container');
 let edit = document.querySelector('.profile__edit');
 let close = document.querySelector('.popup__close');
 let save = document.querySelector('.popup__save');
 let popup = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__name');
-let popupRole = document.querySelector('.popup__role');
+let popupName = document.querySelector('.popup__text_name');
+let popupRole = document.querySelector('.popup__text_role');
 let profileName = document.querySelector('.profile__name');
 let profileRole = document.querySelector('.profile__role');
 
+
 function openPopup(){
     // popup.style.display = "block";
-
     popup.classList.remove('popup_hidden');
     popupName.value = profileName.textContent;
     popupRole.value = profileRole.textContent;
-
 }
 
 function closePopup(){
     popup.classList.add('popup_hidden');
 }
 
-function savePopup(){ 
-    e.preventDefault();
+function savePopup(event){ 
+    event.preventDefault();
     closePopup();
     profileName.textContent = popupName.value;
     profileRole.textContent = popupRole.value;
-
 }
-
 
 edit.addEventListener('click', openPopup);
 close.addEventListener('click', closePopup);
-save.addEventListener('click', savePopup);
+form.addEventListener('submit', savePopup);

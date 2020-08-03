5072a2ae97cb8827b64eb31bd3deb5450dcd350a
Elena <lenhenlady@gmail.com>
2020-08-03 09:31:54 +0300

    third review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,17 @@
 let form = document.querySelector('.popup__container');
 let edit = document.querySelector('.profile__edit');
 let close = document.querySelector('.popup__close');
-let save = document.querySelector('.popup__save');
 let popup = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__text_name');
-let popupRole = document.querySelector('.popup__text_role');
+let popupName = document.querySelector('.popup__text_type_name');
+let popupRole = document.querySelector('.popup__text_type_role');
 let profileName = document.querySelector('.profile__name');
 let profileRole = document.querySelector('.profile__role');
 
 
-function openPopup(){
-    // popup.style.display = "block";
-    popup.classList.remove('popup_hidden');
+function openPopup(){    
     popupName.value = profileName.textContent;
     popupRole.value = profileRole.textContent;
+    popup.classList.remove('popup_hidden');
 }
 
 function closePopup(){
@@ -22,10 +20,10 @@ function closePopup(){
 }
 
 function savePopup(event){ 
-    event.preventDefault();
-    closePopup();
+    event.preventDefault();    
     profileName.textContent = popupName.value;
     profileRole.textContent = popupRole.value;
+    closePopup();
 }
 
 edit.addEventListener('click', openPopup);
